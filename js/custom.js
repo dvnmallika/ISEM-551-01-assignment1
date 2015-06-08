e387cfc0af9fdc51b1de44dc1b73caef24fea065
Venkata Naga Mallika Devireddy <dvnmallika@gmail.com>
2015-06-07 22:57:24 -0400

    Adding assignment 1

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+ $("form").submit(function(){
+	 // This is called after basic HTML 5 validations (Required)  on Email and Password fields . 
+	 alert("This Function has not been implemented yet, Thank you for visiting.");
+});
